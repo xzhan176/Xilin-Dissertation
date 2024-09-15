e13f66c098d90cb1a04ee86655101be069a2c9bc
Hung Nguyen <nthungdev@gmail.com>
2024-09-14 23:04:40 -0400

    rename function arguments

--- K-node-Hung/polarization_functions/obj.py
+++ K-node-Hung/polarization_functions/obj.py
@@ -1,10 +1,10 @@
 import numpy as np
 from utils import mean_center
 
-def fn(A, op, n) -> float:
+def fn(A, opinions, n) -> float:
     """
     Maximizing polarization only: \\bar{z}^T \\bar{z}
     """
-    op_mean = mean_center(op, n)
+    op_mean = mean_center(opinions, n)
     z_mean = np.dot(A, op_mean)
     return np.dot(np.transpose(z_mean), z_mean)[0, 0]
